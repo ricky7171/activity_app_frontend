6f2485a223320d6f473c50ae96045004c1f1fed0
azam <pravodev@gmail.com>
2021-07-19 11:07:05 +0700

    load setting and save to global window variable

--- js/app/pages/all.js
+++ js/app/pages/all.js
@@ -1,4 +1,5 @@
 import * as dateTimeHelper from "./../core/datetime_helper";
+import * as settingRepository from '../data/setting_repository';
 
 
 function changeReportTextToCurrentMonth()
@@ -9,6 +10,16 @@ function changeReportTextToCurrentMonth()
     $("#reportBtnTop").html(dateTimeHelper.getCurrentMonth() + " Report").attr("href", "/report/list.html?year=" + currentYear + "&month=" + currentMonth);
 }
 
+async function setSettingObject()
+{
+    var result = await settingRepository.getSetting()
+
+    if(result.response.data) {
+        window.setting = result.response.data;
+    }
+} 
+
 jQuery(async function () {
     changeReportTextToCurrentMonth();
+    setSettingObject();
 })
